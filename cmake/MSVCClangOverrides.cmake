79e64164bf31c0671f60c5bd6eef8b8dd89e1b82
tizderub <tiziano.derubeis@atscom.it>
2017-05-12 17:17:49 +0200

    Adding /GR (enable RTTI language) in CLANG

--- cmake/MSVCClangOverrides.cmake
+++ cmake/MSVCClangOverrides.cmake
@@ -1,5 +1,5 @@
 ## FLAGS FOR CLANG FOR WINDOWS
-set(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MTd -Od -g2 -gdwarf-2 -std=c++1z -fms-extensions -fexceptions")
-set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT -O1 /D NDEBUG -std=c++1z -fms-extensions -fexceptions")
-set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT -O3 /D NDEBUG -std=c++1z -fms-extensions -fexceptions")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT -O2 /D NDEBUG -g2 -gdwarf-2 -std=c++1z -fms-extensions -fexceptions")
+set(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MTd /GR -Od -g2 -gdwarf-2 -std=c++1z -fms-extensions -fexceptions")
+set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /GR -O1 /D NDEBUG -std=c++1z -fms-extensions -fexceptions")
+set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /GR -O3 /D NDEBUG -std=c++1z -fms-extensions -fexceptions")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /GR -O2 /D NDEBUG -g2 -gdwarf-2 -std=c++1z -fms-extensions -fexceptions")
